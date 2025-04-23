d92bc45382a1bfc2e8c4a510d06accf80414bcd1
nacho-taime <naho.tai.0516@gmail.com>
2025-04-24 06:57:22 +0900

    5章課題のアップロード

---
+++ kadai_005/score.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP3〜5章課題</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 変数を作成し、値を代入する
+    $score1 = 80;
+    $score2 = 60;
+    $score3 = 55;
+    $score4 = 40;
+    $score5 = 100;
+    $score6 = 25;
+    $score7 = 80;
+    $score8 = 95;
+    $score9 = 30;
+    $score10 = 60;
+
+    // 合計点と平均点を算出する
+    $total = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+    echo $total / 10;
+    ?>
+  </p>
+
+</body>
+</html>
