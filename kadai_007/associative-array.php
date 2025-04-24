a3c137ef720c4ea70150c320902ec6d0e2f0ca88
nacho-taime <naho.tai.0516@gmail.com>
2025-04-25 06:20:33 +0900

    7章課題のアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>7章課題</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 連想配列を作成する
+    $products_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    // 値を出力する
+    print_r($products_data);
+    ?>
+  </p>
+</body>
+</html>
